61405d93fae039c217856bdaa2b0e126fc912855
Nguyễn Tiến Đạt <datnt114@viettel.com.vn>
2022-10-31 16:18:38 +0700

    Fix get all labels in data

--- get_class_json.py
+++ get_class_json.py
@@ -1,24 +1,27 @@
 import glob
 import json
 import os
+from pathlib import Path
 
 
-def get_classes_json(labels_folder="data/labels", json_file_name="classes.json"):
+def get_classes_json(folder="data/", json_file_name="classes.json"):
     classes = {}
     # if not os.path.exists(f"data/{json_file_name}"):
     i = 0
-    for file in glob.glob(labels_folder + "/*.txt"):
-        label_file = open(file, "r")
-        label = label_file.readline().strip('\n')
-        if not (label in classes.keys()):
-            classes.update({
-                label: str(i)
-            })
-            i += 1
+    for phase in ['training', 'testing', 'validation' ]:
+        path_label = folder + phase
+        for file in glob.glob(path_label + "/labels/*.txt"):
+            label_file = open(file, "r")
+            label = label_file.readline().strip('\n')
+            if not (label in classes.keys()):
+                classes.update({
+                    label: str(i)
+                })
+                i += 1
 
     with open(f'data/{json_file_name}', 'w', encoding='utf-8') as f:
         json.dump(classes, f, ensure_ascii=False, indent=4)
 
 
 if __name__ == "__main__":
-    get_classes_json(labels_folder="data/training/labels")
+    get_classes_json(folder="data/")
